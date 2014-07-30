32be7faf54aa71f29d633b6fee79a705f7b1c348
Chris N <swordsman9613@gmail.com>
2014-07-29 17:46:46 -1000

    Added circular saw support for moreblocks
    Gotta port this over to SkyLands proper now...

---
+++ mods/skylands/stairsplus.lua
@@ -0,0 +1,55 @@
+stairsplus:register_all("skylands", "cinder", "skylands:cinder_block", {
+		groups = {cracky=3, crumbly=1},
+		tiles = {"skylands_cinder_block.png"},
+		description = "Cinder Block",
+		light_source = 0
+	})
+
+stairsplus:register_all("skylands", "white_stone", "skylands:white_stone", {
+		groups = {cracky=3},
+		tiles = {"skylands_white_stone.png"},
+		description = "White Stone",
+		light_source = 0
+	})
+
+stairsplus:register_all("skylands", "white_cobble", "skylands:white_cobble", {
+		groups = {cracky=3},
+		tiles = {"skylands_white_cobble.png"},
+		description = "White Cobblestone",
+		light_source = 0
+	})
+
+stairsplus:register_all("skylands", "white_stone_brick", "skylands:white_stone_brick", {
+		groups = {cracky=2},
+		tiles = {"skylands_white_stone_brick.png"},
+		description = "White Stone Brick",
+		light_source = 0
+	})
+	
+stairsplus:register_all("skylands", "quartz", "skylands:quartz", {
+		groups = {cracky=2},
+		tiles = {"skylands_quartz.png"},
+		description = "Quartz",
+		light_source = 0
+	})
+
+stairsplus:register_all("skylands", "quartz_pillar", "skylands:quartz_pillar", {
+		groups = {cracky=2},
+		tiles = {"skylands_quartz_pillar.png"},
+		description = "Quartz Pillar",
+		light_source = 0
+	})
+
+stairsplus:register_all("skylands", "acaciawood", "skylands:acaciawood", {
+		groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+		tiles = {"skylands_acaciawood.png"},
+		description = "Acacia Wood Planks",
+		light_source = 0
+	})
+	
+stairsplus:register_all("skylands", "pinewood", "skylands:pinewood", {
+		groups = {choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+		tiles = {"skylands_pinewood.png"},
+		description = "Pine Wood Planks",
+		light_source = 0
+	})
